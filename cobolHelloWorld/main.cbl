44b712315907f6ede71bde57be6ae182d719b382
PJennings <PJennings@host.docker.internal>
2022-02-15 17:08:07 +0000

    added main.cbl

---
+++ cobolHelloWorld/main.cbl
@@ -0,0 +1,19 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. main.
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+       
+       Main-Paragraph.
+       
+           display "Hello World".
+           display " ".
+           
+           Stop Run.
+
+       END PROGRAM main.
